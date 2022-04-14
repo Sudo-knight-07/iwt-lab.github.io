3262ee6b36958c846490ac50894830617eac1430
Vaibhav <nightwalker.alon@gmail.com>
2022-04-14 22:48:34 +0530

    version1
    this version is created for assignment 2

---
+++ index.js
@@ -0,0 +1,15 @@
+function openNav() {
+  document.getElementById("mySidenav1").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav1").style.width = "0";
+}
+
+function openNav2() {
+  document.getElementById("mySidenav2").style.width = "250px";
+}
+
+function closeNav2() {
+  document.getElementById("mySidenav2").style.width = "0";
+}
